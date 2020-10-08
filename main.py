3ee4feaf477d88a0c75d5426d5838903e7f699ad
Batuhan-Durhan35 <71413827+Batuhan-Durhan35@users.noreply.github.com>
2020-10-08 14:23:13 -0400

    Update pxt.json, main.ts, main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+# Created by: Batuhan
+# Created at: 8 October 2020
 game.splash("Pizza Order. ")
 diameter = game.ask_for_number("Enter the diameter of the Pizza (inch).")
 cost = 0.75 + (1 + 0.5 * diameter)
-game.splash("The cost for " + str(diameter) + " inch pizza is $" + str(cost * 1.13) + str(Math.round(cost)))
+game.splash("The cost for " + str(diameter) + " inch pizza is $" + str(cost * 1.13) + "(HST included).")
